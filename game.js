4e001f18521ebc2de1e24b23d6878df8dedd4e08
allimenke <alexandra.menke1@marist.edu>
2013-11-20 20:28:01 -0500

    Pick prince
    pick prince function along with story line

--- game.js
+++ game.js
@@ -15,6 +15,19 @@ document.getElementById('startPage').style.display='block';
 document.getElementById('helpPage').style.display='block';
 }
 
+function pickPrince()
+{
+document.getElementById('storyline').innerHTML="   Once upon a time in medieval times, '+
+'there was a war.  It was between Allitopia and Plante Castle(the enemy).  It was a long '+
+'and deadly war.  One day the Plante knights captured the princess of Allitopia and took '+
+'her back to the castle and threw her in the dungeon.  In order to make sure she didn’t '+
+'escape, they made the vicious dragon guard the dungeon door.  Once the prince found out, '+
+'the game begins. "
+         
+}
+
+
+
 function help()
 {
 alert('To play the game you must first pick the name of the prince that will help save the princess. '+
