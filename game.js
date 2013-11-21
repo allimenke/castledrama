667cfb4e52cfb093c54301e87769f98a635dec91
allimenke <alexandra.menke1@marist.edu>
2013-11-20 19:34:14 -0500

    Revert "Add to change pages"
    This reverts commit 94bb7b560951953a47dd845ed4b93f67d80e14b9.

---
+++ game.js
@@ -0,0 +1,26 @@
+function start()
+{
+document.getElementById('startButton').style.visibility="hidden";
+document.getElementById('outputdiv').innerHTML="<input type=\"text\" id=\"name\" size=15 value=\"\">"
+}
+function enterCastle()
+{
+
+}
+function firstRoomChoice(firstChoice)
+{
+if(firstChoice==='Right')
+{
+
+}
+function help()
+{
+alert('To play the game you must first pick the name of the prince that will help save the princess. '+
+'You will have to navigate through each room but choosing if you would like to run, fight, or pick up certain items. '+
+'When you encounter different enemies, only certain items will defeat them. '+ 'Best of luck saving the princess!')
+}
+
+
+
+}
+ 
