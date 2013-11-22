7f629ee83c79d836221de50c30343b6191aba942
allimenke <alexandra.menke1@marist.edu>
2013-11-22 13:07:52 -0500

    Sword vis

--- game.js
+++ game.js
@@ -31,11 +31,13 @@ alert('To play the game you must first pick the name of the prince that will hel
 function swordTrue()
 {
 swordYes=document.getElementById('bridgeRight').value=true;
+document.getElementById('bridgeRight').style.display='block';
 }
 
 function swordFalse()
 {
 swordNo=document.getElementById('bridgeRight').value=false;
+document.getElementById('bridgeRight').style.display='block';
 }
 
 
